e9c5f6ea0e2b03bbbf99414686e5ff5f02326780
Katie Reed <katharynreedpierce@gmail.com>
2017-03-03 08:51:28 -0800

    create passing test conditions for getAll and save in brand model

--- src/brand.php
+++ src/brand.php
@@ -21,11 +21,30 @@
             return $this->id;
         }
 
-        static function deleteAll()
-    {
-        $GLOBALS['DB']->exec("DELETE FROM brands;");
-    }
+        function save()
+        {
+            $exec = $GLOBALS['DB']->prepare("INSERT INTO brands (name) VALUES (:name)");
+            $exec->execute([':name' => $this->getName()]);
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
 
+        static function getAll()
+        {
+           $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+           $brands = [];
+           foreach ($returned_brands as $brand) {
+               $name = $brand['name'];
+               $id = $brand['id'];
+               $new_brand = new Brand ($name, $id);
+               array_push($brands, $new_brand);
+           }
+           return $brands;
+       }
+
+       static function deleteAll()
+       {
+           $GLOBALS['DB']->exec("DELETE FROM brands;");
+       }
 
 
     }
--- tests/brandTest.php
+++ tests/brandTest.php
@@ -28,6 +28,22 @@ class BrandTest extends PHPUnit_Framework_TestCase
         $expected_result = array('Soles of the Damned', 1);
         $this->assertEquals($result, $expected_result);
     }
+
+    function test_save_getAll()
+    {
+        // Arrange
+        $name_one = 'Soles of the Damned';
+        $test_brand_one = new Brand ($name_one);
+        $test_brand_one->save();
+        $name_two = 'Cleft-Toe Maniac';
+        $test_brand_two = new Brand ($name_two);
+        $test_brand_two->save();
+        // Act
+        $result = Brand::getAll();
+        $expected_result = array($test_brand_one, $test_brand_two);
+        // Assert
+        $this->assertEquals($result, $expected_result);
+    }
 }
 
 
