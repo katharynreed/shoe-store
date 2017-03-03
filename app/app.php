13a105d3b7306deba8679eb183e98f74cf742ce0
Katie Reed <katharynreedpierce@gmail.com>
2017-03-03 11:26:38 -0800

    integrate CRUD to sliex

--- app/app.php
+++ app/app.php
@@ -17,13 +17,51 @@
     Request::enableHttpMethodParameterOverride();
     $app['debug'] = true;
 
-    $app->get('/', function() use($app) {
+    $app->get('/', function() use ($app) {
         $stores = Store::getAll();
-        return $app["twig"]->render("root.html.twig", ['store' => $store]);
+        return $app['twig']->render('homepage.html.twig', ['store' => $store]);
+    });
+
+
+    $app->get('/stores', function($id) use ($app) {
+        $stores = Store::getAll();
+        $brands = Brand::getAll();
+        return $app['twig']->render('store.html.twig', ['stores' => $stores, 'brands' => $brands]);
+    });
+
+    $app->post('/add_store', function() use ($app) {
+        $name = $_POST['name'];
+        $new_store = new Store ($name);
+        $new_store->save();
+        return $app->redirect('/stores');
+    });
+
+    $app->get('/stores/{id}', function($id) use ($app) {
+        $stores = Store::find($id);
+        $brands = Brand::getAll();
+        return $app['twig']->render('store.html.twig', ['stores' => $stores, 'brands' => $brands]);
+    });
+
+    $app->post('/stores/{id}/edit', function($id) use ($app) {
+        $store = Store::find($id);
+        $new_name = $_POST['new-name'];
+
+        $store->updateName($new_name);
+
+        return $app->redirect('/stores/'.$id);
+    });
+
+    $app->delete('/stores/{id}/delete', function($id) use ($app) {
+        $store = Store::find($id);
+        $store->delete();
+
+        return $app->redirect('/stores');
     });
 
 
 
 
 
+
+return $app;
 ?>
