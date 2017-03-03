b25616a9fdf47bea09aef5b50d7941c736c896ea
Katie Reed <katharynreedpierce@gmail.com>
2017-03-03 08:54:48 -0800

    create passing test condition for find in brand model

--- src/brand.php
+++ src/brand.php
@@ -28,22 +28,34 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        static function find($id)
+       {
+            $found_brand;
+            $brands = Brand::getAll();
+            foreach ($brands as $brand) {
+                if ($brand->getId() == $id) {
+                   $found_brand = $brand;
+                }
+            }
+            return $found_brand;
+       }
+
         static function getAll()
         {
-           $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
-           $brands = [];
-           foreach ($returned_brands as $brand) {
-               $name = $brand['name'];
-               $id = $brand['id'];
-               $new_brand = new Brand ($name, $id);
-               array_push($brands, $new_brand);
-           }
-           return $brands;
-       }
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            $brands = [];
+            foreach ($returned_brands as $brand) {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand ($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
 
        static function deleteAll()
        {
-           $GLOBALS['DB']->exec("DELETE FROM brands;");
+            $GLOBALS['DB']->exec("DELETE FROM brands;");
        }
 
 
--- tests/brandTest.php
+++ tests/brandTest.php
@@ -35,6 +35,7 @@ class BrandTest extends PHPUnit_Framework_TestCase
         $name_one = 'Soles of the Damned';
         $test_brand_one = new Brand ($name_one);
         $test_brand_one->save();
+
         $name_two = 'Cleft-Toe Maniac';
         $test_brand_two = new Brand ($name_two);
         $test_brand_two->save();
@@ -43,6 +44,22 @@ class BrandTest extends PHPUnit_Framework_TestCase
         $expected_result = array($test_brand_one, $test_brand_two);
         // Assert
         $this->assertEquals($result, $expected_result);
+    }
+
+    function test_find()
+    {
+        // Arrange
+        $name_one = 'Soles of the Damned';
+        $test_brand_one = new Brand ($name_one);
+        $test_brand_one->save();
+
+        $name_two = 'Cleft-Toe Maniac';
+        $test_brand_two = new Brand ($name_two);
+        $test_brand_two->save();
+        // Act
+        $result = Brand::find($test_brand_one->getId());
+        // Assert
+        $this->assertEquals($test_brand_one, $result);
     }
 }
 
